3b530d8658492f34bf1d7b50ccf9df02ba2507cb
Kevin Cernekee <cernekee@gmail.com>
2014-03-13 22:59:59 -0700

    build-debian.sh: Automate PPA builds
    If this script is run on an Ubuntu system, it will generate an appropriate
    changelog entry and name the new version something like
    "0.5~20140313-1ubuntu1~precise".  The first tilde will cause it to be
    superseded by "0.5"[1].  The resultant *.changes file can be directly
    uploaded to PPA/Launchpad.
    
    Debian sid behavior is unchanged, as we will continue to use sid for
    official uploads.
    
    [1] https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-Version
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- build-debian.sh
+++ build-debian.sh
@@ -2,6 +2,7 @@
 
 gpgkey="BC0B0D65"
 builddir=tmp.debian
+pkg=stoken
 
 set -ex
 
@@ -13,9 +14,10 @@ function build_one
 	mkdir $builddir
 	pushd $builddir
 
-	cp ../$tarball stoken_${ver}.orig.tar.gz
-	tar zxf ../$tarball
-	cd stoken-${ver}
+	cp ../$tarball "${pkg}_${ver}.orig.tar.gz"
+	mkdir "$pkg-$ver"
+	cd "$pkg-$ver"
+	tar --strip 1 -zxf ../../$tarball
 	cp -a ../../debian .
 	if [ "$nosign" = "0" ]; then
 		debuild "$arg"
@@ -31,7 +33,7 @@ function build_one
 # MAIN
 #
 
-tarball=$(ls -1 stoken-*.tar.gz 2> /dev/null || true)
+tarball=$(ls -1 ${pkg}-*.tar.gz 2> /dev/null || true)
 if [ -z "$tarball" -o ! -e "$tarball" ]; then
 	echo "missing release tarball"
 	exit 1
@@ -46,15 +48,33 @@ else
 	nosign=1
 fi
 
-rm -f lintian.txt stoken*.deb
+rm -f lintian.txt ${pkg}*.deb
 touch lintian.txt
+
+dist=$(lsb_release -si)
+if [ "$dist" = "Ubuntu" ]; then
+	git checkout -f debian/changelog
+	codename=$(lsb_release -sc)
+	today=$(date +%Y%m%d)
+	ver="${ver}~${today}"
+	incver=${incver:-1}
+	dch --newversion "$ver-1ubuntu${incver}~$codename" \
+		--distribution $codename \
+		--force-bad-version \
+		"New PPA build."
+fi
 
 build_one ""
 cp $builddir/*.deb .
 echo "------------" >> lintian.txt
 build_one "-S"
 
+set +ex
+
+echo "--------"
 echo "lintian:"
+echo "--------"
 cat lintian.txt
+echo "--------"
 
 exit 0
