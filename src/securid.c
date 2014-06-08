dabbff5573024345c4d23e8ea15bf70c68b1b563
Kevin Cernekee <cernekee@gmail.com>
2014-06-08 11:12:48 -0700

    securid: Reorganize file
    Move the public functions to the end, and improve consistency between the
    v2 and v3 implementations.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- src/securid.c
+++ src/securid.c
@@ -283,6 +283,275 @@ static void sha256_pbkdf2(const uint8_t *pass, int pass_len,
 }
 
 /********************************************************************
+ * V1/V2 token handling
+ ********************************************************************/
+
+static void numinput_to_bits(const char *in, uint8_t *out, unsigned int n_bits)
+{
+	int bitpos = 13;
+
+	memset(out, 0, (n_bits + 7) / 8);
+	for (; n_bits; n_bits -= TOKEN_BITS_PER_CHAR, in++) {
+		uint16_t decoded = (*in - '0') & 0x07;
+		decoded <<= bitpos;
+		out[0] |= decoded >> 8;
+		out[1] |= decoded & 0xff;
+
+		bitpos -= TOKEN_BITS_PER_CHAR;
+		if (bitpos < 0) {
+			bitpos += 8;
+			out++;
+		}
+	}
+}
+
+static void bits_to_numoutput(const uint8_t *in, char *out, unsigned int n_bits)
+{
+	int bitpos = 13;
+
+	for (; n_bits; n_bits -= TOKEN_BITS_PER_CHAR, out++) {
+		uint16_t binary = (in[0] << 8) | in[1];
+		*out = ((binary >> bitpos) & 0x07) + '0';
+
+		bitpos -= TOKEN_BITS_PER_CHAR;
+		if (bitpos < 0) {
+			bitpos += 8;
+			in++;
+		}
+	}
+	*out = 0;
+}
+
+static uint32_t get_bits(const uint8_t *in, unsigned int start, int n_bits)
+{
+	uint32_t out = 0;
+
+	in += start / 8;
+	start %= 8;
+
+	for (; n_bits > 0; n_bits--) {
+		out <<= 1;
+		if ((*in << start) & 0x80)
+			out |= 0x01;
+		start++;
+		if (start == 8) {
+			start = 0;
+			in++;
+		}
+	}
+	return out;
+}
+
+static void set_bits(uint8_t *out, unsigned int start, int n_bits, uint32_t val)
+{
+	out += start / 8;
+	start %= 8;
+	val <<= (32 - n_bits);
+
+	for (; n_bits > 0; n_bits--) {
+		if (val & BIT(31))
+			*out |= BIT(7 - start);
+		else
+			*out &= ~BIT(7 - start);
+		val <<= 1;
+		start++;
+		if (start == 8) {
+			start = 0;
+			out++;
+		}
+	}
+}
+
+static int v2_decode_token(const char *in, struct securid_token *t)
+{
+	uint8_t d[MAX_TOKEN_BITS / 8 + 2];
+	int len = strlen(in);
+	uint16_t token_mac, computed_mac;
+
+	if (len < MIN_TOKEN_CHARS || len > MAX_TOKEN_CHARS)
+		return ERR_BAD_LEN;
+
+	/* the last 5 digits provide a checksum for the rest of the string */
+	numinput_to_bits(&in[len - CHECKSUM_CHARS], d, 15);
+	token_mac = get_bits(d, 0, 15);
+	computed_mac = securid_shortmac(in, len - CHECKSUM_CHARS);
+
+	if (token_mac != computed_mac)
+		return ERR_CHECKSUM_FAILED;
+
+	t->version = in[0] - '0';
+	memcpy(&t->serial, &in[VER_CHARS], SERIAL_CHARS);
+	t->serial[SERIAL_CHARS] = 0;
+
+	numinput_to_bits(&in[BINENC_OFS], d, BINENC_BITS);
+	memcpy(t->enc_seed, d, AES_KEY_SIZE);
+	t->has_enc_seed = 1;
+
+	t->flags = get_bits(d, 128, 16);
+	t->exp_date = get_bits(d, 144, 14);
+	t->dec_seed_hash = get_bits(d, 159, 15);
+	t->device_id_hash = get_bits(d, 174, 15);
+
+	return ERR_NONE;
+}
+
+static int generate_key_hash(uint8_t *key_hash, const char *pass,
+	const char *devid, uint16_t *device_id_hash, struct securid_token *t)
+{
+	uint8_t key[MAX_PASS + DEVID_CHARS + MAGIC_LEN + 1], *devid_buf;
+	int pos = 0, devid_len = t->is_smartphone ? 40 : 32;
+	const uint8_t magic[] = { 0xd8, 0xf5, 0x32, 0x53, 0x82, 0x89, 0x00 };
+
+	memset(key, 0, sizeof(key));
+
+	if (pass) {
+		pos = strlen(pass);
+		if (pos > MAX_PASS)
+			return ERR_BAD_PASSWORD;
+		memcpy(key, pass, pos);
+	}
+
+	devid_buf = &key[pos];
+	if (devid) {
+		int len = 0;
+
+		/*
+		 * For iPhone/Android ctf strings, the device ID takes up
+		 * 40 bytes and consists of hex digits + zero padding.
+		 *
+		 * For other ctf strings (e.g. --blocks), the device ID takes
+		 * up 32 bytes and consists of decimal digits + zero padding.
+		 *
+		 * If this seed isn't locked to a device, we'll just hash
+		 * 40 (or 32) zero bytes, below.
+		 */
+		for (; *devid; devid++) {
+			if (++len > devid_len)
+				break;
+			if ((t->version == 1 && isdigit(*devid)) ||
+			    (t->version >= 2 && !isxdigit(*devid)))
+				continue;
+			key[pos++] = toupper(*devid);
+		}
+	}
+	if (device_id_hash)
+		*device_id_hash = securid_shortmac(devid_buf, devid_len);
+
+	memcpy(&key[pos], magic, MAGIC_LEN);
+	securid_mac(key, pos + MAGIC_LEN, key_hash);
+
+	return ERR_NONE;
+}
+
+static int v2_decrypt_seed(struct securid_token *t, const char *pass,
+			   const char *devid)
+{
+	uint8_t key_hash[AES_BLOCK_SIZE], dec_seed_hash[AES_BLOCK_SIZE];
+	uint16_t computed_mac;
+	uint16_t device_id_hash;
+	int rc;
+
+	if (t->flags & FL_PASSPROT && !pass)
+		return ERR_MISSING_PASSWORD;
+	if (t->flags & FL_SNPROT && !devid)
+		return ERR_MISSING_PASSWORD;
+
+	rc = generate_key_hash(key_hash,
+			       t->flags & FL_PASSPROT ? pass : NULL,
+			       t->flags & FL_SNPROT ? devid : NULL,
+			       &device_id_hash, t);
+	if (rc)
+		return rc;
+
+	if (t->flags & FL_SNPROT && device_id_hash != t->device_id_hash)
+		return ERR_BAD_DEVID;
+
+	aes128_ecb_decrypt(key_hash, t->enc_seed, t->dec_seed);
+	securid_mac(t->dec_seed, AES_KEY_SIZE, dec_seed_hash);
+	computed_mac = (dec_seed_hash[0] << 7) | (dec_seed_hash[1] >> 1);
+
+	if (computed_mac != t->dec_seed_hash)
+		return ERR_DECRYPT_FAILED;
+	t->has_dec_seed = 1;
+
+	return ERR_NONE;
+}
+
+static void key_from_time(const uint8_t *bcd_time, int bcd_time_bytes,
+	const uint8_t *serial, uint8_t *key)
+{
+	int i;
+
+	memset(key, 0xaa, 8);
+	memcpy(key, bcd_time, bcd_time_bytes);
+	memset(key + 12, 0xbb, 4);
+
+	/* write BCD-encoded partial serial number */
+	key += 8;
+	for (i = 4; i < 12; i += 2)
+		*(key++) = ((serial[i] - '0') << 4) |
+			    (serial[i + 1] - '0');
+}
+
+static void bcd_write(uint8_t *out, int val, unsigned int bytes)
+{
+	out += bytes - 1;
+	for (; bytes; bytes--) {
+		*out = val % 10;
+		val /= 10;
+		*(out--) |= (val % 10) << 4;
+		val /= 10;
+	}
+}
+
+static int v2_encode_token(const struct securid_token *t, const char *pass,
+			   const char *devid, char *out)
+{
+	struct securid_token newt = *t;
+	uint8_t d[MAX_TOKEN_BITS / 8 + 2];
+	uint8_t key_hash[AES_BLOCK_SIZE];
+	int rc;
+
+	/* empty password means "no password" */
+	if (pass && !strlen(pass))
+		pass = NULL;
+	if (devid && !strlen(devid))
+		devid = NULL;
+
+	rc = generate_key_hash(key_hash, pass, devid, &newt.device_id_hash,
+			       &newt);
+	if (rc)
+		return rc;
+
+	if (pass)
+		newt.flags |= FL_PASSPROT;
+	else
+		newt.flags &= ~FL_PASSPROT;
+
+	if (devid)
+		newt.flags |= FL_SNPROT;
+	else
+		newt.flags &= ~FL_SNPROT;
+
+	memset(d, 0, sizeof(d));
+	aes128_ecb_encrypt(key_hash, newt.dec_seed, newt.enc_seed);
+	memcpy(d, newt.enc_seed, AES_KEY_SIZE);
+
+	set_bits(d, 128, 16, newt.flags);
+	set_bits(d, 144, 14, newt.exp_date);
+	set_bits(d, 159, 15, securid_shortmac(newt.dec_seed, AES_KEY_SIZE));
+	set_bits(d, 174, 15, newt.device_id_hash);
+
+	sprintf(out, "%d%s", newt.version, newt.serial);
+	bits_to_numoutput(d, &out[BINENC_OFS], BINENC_BITS);
+
+	set_bits(d, 0, 15, securid_shortmac(out, CHECKSUM_OFS));
+	bits_to_numoutput(d, &out[CHECKSUM_OFS], CHECKSUM_BITS);
+
+	return ERR_NONE;
+}
+
+/********************************************************************
  * V3 token handling
  ********************************************************************/
 
@@ -317,7 +586,7 @@ static void v3_derive_key(const char *pass, const char *devid, const uint8_t *sa
 	sha256_pbkdf2(buf1, buf_len >> 1, salt, V3_NONCE_BYTES, 1000, out);
 }
 
-static int v3_decode(const char *in, struct securid_token *t)
+static int v3_decode_token(const char *in, struct securid_token *t)
 {
 	char decoded[BASE64_MIN_INPUT + 1];
 	int i, j;
@@ -404,8 +673,8 @@ static void v3_compute_hmac(struct v3_token *v3, const char *pass,
 		    (void *)v3, sizeof(*v3) - SHA256_HASH_SIZE, out);
 }
 
-static int v3_decrypt(struct securid_token *t,
-		      const char *pass, const char *devid)
+static int v3_decrypt_seed(struct securid_token *t,
+			   const char *pass, const char *devid)
 {
 	struct v3_payload payload;
 	uint8_t hash[SHA256_HASH_SIZE];
@@ -449,242 +718,37 @@ static int v3_decrypt(struct securid_token *t,
 }
 
 /********************************************************************
- * V1/V2 token handling
+ * Public functions
  ********************************************************************/
-
-static void numinput_to_bits(const char *in, uint8_t *out, unsigned int n_bits)
-{
-	int bitpos = 13;
-
-	memset(out, 0, (n_bits + 7) / 8);
-	for (; n_bits; n_bits -= TOKEN_BITS_PER_CHAR, in++) {
-		uint16_t decoded = (*in - '0') & 0x07;
-		decoded <<= bitpos;
-		out[0] |= decoded >> 8;
-		out[1] |= decoded & 0xff;
-
-		bitpos -= TOKEN_BITS_PER_CHAR;
-		if (bitpos < 0) {
-			bitpos += 8;
-			out++;
-		}
-	}
-}
-
-static void bits_to_numoutput(const uint8_t *in, char *out, unsigned int n_bits)
-{
-	int bitpos = 13;
-
-	for (; n_bits; n_bits -= TOKEN_BITS_PER_CHAR, out++) {
-		uint16_t binary = (in[0] << 8) | in[1];
-		*out = ((binary >> bitpos) & 0x07) + '0';
-
-		bitpos -= TOKEN_BITS_PER_CHAR;
-		if (bitpos < 0) {
-			bitpos += 8;
-			in++;
-		}
-	}
-	*out = 0;
-}
-
-static uint32_t get_bits(const uint8_t *in, unsigned int start, int n_bits)
-{
-	uint32_t out = 0;
-
-	in += start / 8;
-	start %= 8;
-
-	for (; n_bits > 0; n_bits--) {
-		out <<= 1;
-		if ((*in << start) & 0x80)
-			out |= 0x01;
-		start++;
-		if (start == 8) {
-			start = 0;
-			in++;
-		}
-	}
-	return out;
-}
-
-static void set_bits(uint8_t *out, unsigned int start, int n_bits, uint32_t val)
-{
-	out += start / 8;
-	start %= 8;
-	val <<= (32 - n_bits);
-
-	for (; n_bits > 0; n_bits--) {
-		if (val & BIT(31))
-			*out |= BIT(7 - start);
-		else
-			*out &= ~BIT(7 - start);
-		val <<= 1;
-		start++;
-		if (start == 8) {
-			start = 0;
-			out++;
-		}
-	}
-}
 
 int securid_decode_token(const char *in, struct securid_token *t)
 {
-	uint8_t d[MAX_TOKEN_BITS / 8 + 2];
-	int len = strlen(in);
-	uint16_t token_mac, computed_mac;
-
-	/* V3 token always starts with a base64-encoded 0x03 byte */
-	if (len >= BASE64_MIN_INPUT && (in[0] == 'A'))
-		return v3_decode(in, t);
-
-	if (len < MIN_TOKEN_CHARS || len > MAX_TOKEN_CHARS)
-		return ERR_BAD_LEN;
-
-	/* we can handle v1 or v2 tokens */
-	if (in[0] != '1' && in[0] != '2')
+	/*
+	 * V1/V2 tokens start with the ASCII version digit
+	 * V3 tokens always start with a base64-encoded 0x03 byte, which
+	 *   is guaranteed to encode to 'A'
+	 */
+	if (in[0] == '1' || in[0] == '2')
+		return v2_decode_token(in, t);
+	else if (strlen(in) >= BASE64_MIN_INPUT && (in[0] == 'A'))
+		return v3_decode_token(in, t);
+	else
 		return ERR_TOKEN_VERSION;
-
-	/* the last 5 digits provide a checksum for the rest of the string */
-	numinput_to_bits(&in[len - CHECKSUM_CHARS], d, 15);
-	token_mac = get_bits(d, 0, 15);
-	computed_mac = securid_shortmac(in, len - CHECKSUM_CHARS);
-
-	if (token_mac != computed_mac)
-		return ERR_CHECKSUM_FAILED;
-
-	t->version = in[0] - '0';
-	memcpy(&t->serial, &in[VER_CHARS], SERIAL_CHARS);
-	t->serial[SERIAL_CHARS] = 0;
-
-	numinput_to_bits(&in[BINENC_OFS], d, BINENC_BITS);
-	memcpy(t->enc_seed, d, AES_KEY_SIZE);
-	t->has_enc_seed = 1;
-
-	t->flags = get_bits(d, 128, 16);
-	t->exp_date = get_bits(d, 144, 14);
-	t->dec_seed_hash = get_bits(d, 159, 15);
-	t->device_id_hash = get_bits(d, 174, 15);
-
-	return ERR_NONE;
-}
-
-static int generate_key_hash(uint8_t *key_hash, const char *pass,
-	const char *devid, uint16_t *device_id_hash, struct securid_token *t)
-{
-	uint8_t key[MAX_PASS + DEVID_CHARS + MAGIC_LEN + 1], *devid_buf;
-	int pos = 0, devid_len = t->is_smartphone ? 40 : 32;
-	const uint8_t magic[] = { 0xd8, 0xf5, 0x32, 0x53, 0x82, 0x89, 0x00 };
-
-	memset(key, 0, sizeof(key));
-
-	if (pass) {
-		pos = strlen(pass);
-		if (pos > MAX_PASS)
-			return ERR_BAD_PASSWORD;
-		memcpy(key, pass, pos);
-	}
-
-	devid_buf = &key[pos];
-	if (devid) {
-		int len = 0;
-
-		/*
-		 * For iPhone/Android ctf strings, the device ID takes up
-		 * 40 bytes and consists of hex digits + zero padding.
-		 *
-		 * For other ctf strings (e.g. --blocks), the device ID takes
-		 * up 32 bytes and consists of decimal digits + zero padding.
-		 *
-		 * If this seed isn't locked to a device, we'll just hash
-		 * 40 (or 32) zero bytes, below.
-		 */
-		for (; *devid; devid++) {
-			if (++len > devid_len)
-				break;
-			if ((t->version == 1 && isdigit(*devid)) ||
-			    (t->version >= 2 && !isxdigit(*devid)))
-				continue;
-			key[pos++] = toupper(*devid);
-		}
-	}
-	if (device_id_hash)
-		*device_id_hash = securid_shortmac(devid_buf, devid_len);
-
-	memcpy(&key[pos], magic, MAGIC_LEN);
-	securid_mac(key, pos + MAGIC_LEN, key_hash);
-
-	return ERR_NONE;
 }
 
 int securid_decrypt_seed(struct securid_token *t, const char *pass,
-	const char *devid)
+			 const char *devid)
 {
-	uint8_t key_hash[AES_BLOCK_SIZE], dec_seed_hash[AES_BLOCK_SIZE];
-	uint16_t computed_mac;
-	uint16_t device_id_hash;
-	int rc;
-
 	if (t->sdtid)
 		return sdtid_decrypt(t, pass);
-	if (t->v3)
-		return v3_decrypt(t, pass, devid);
-
-	if (t->flags & FL_PASSPROT && !pass)
-		return ERR_MISSING_PASSWORD;
-	if (t->flags & FL_SNPROT && !devid)
-		return ERR_MISSING_PASSWORD;
-
-	rc = generate_key_hash(key_hash,
-			       t->flags & FL_PASSPROT ? pass : NULL,
-			       t->flags & FL_SNPROT ? devid : NULL,
-			       &device_id_hash, t);
-	if (rc)
-		return rc;
-
-	if (t->flags & FL_SNPROT && device_id_hash != t->device_id_hash)
-		return ERR_BAD_DEVID;
-
-	aes128_ecb_decrypt(key_hash, t->enc_seed, t->dec_seed);
-	securid_mac(t->dec_seed, AES_KEY_SIZE, dec_seed_hash);
-	computed_mac = (dec_seed_hash[0] << 7) | (dec_seed_hash[1] >> 1);
-
-	if (computed_mac != t->dec_seed_hash)
-		return ERR_DECRYPT_FAILED;
-	t->has_dec_seed = 1;
-
-	return ERR_NONE;
-}
-
-static void key_from_time(const uint8_t *bcd_time, int bcd_time_bytes,
-	const uint8_t *serial, uint8_t *key)
-{
-	int i;
-
-	memset(key, 0xaa, 8);
-	memcpy(key, bcd_time, bcd_time_bytes);
-	memset(key + 12, 0xbb, 4);
-
-	/* write BCD-encoded partial serial number */
-	key += 8;
-	for (i = 4; i < 12; i += 2)
-		*(key++) = ((serial[i] - '0') << 4) |
-			    (serial[i + 1] - '0');
-}
-
-static void bcd_write(uint8_t *out, int val, unsigned int bytes)
-{
-	out += bytes - 1;
-	for (; bytes; bytes--) {
-		*out = val % 10;
-		val /= 10;
-		*(out--) |= (val % 10) << 4;
-		val /= 10;
-	}
+	else if (t->v3)
+		return v3_decrypt_seed(t, pass, devid);
+	else
+		return v2_decrypt_seed(t, pass, devid);
 }
 
 void securid_compute_tokencode(struct securid_token *t, time_t now,
-	char *code_out)
+			       char *code_out)
 {
 	uint8_t bcd_time[8];
 	uint8_t key0[AES_KEY_SIZE], key1[AES_KEY_SIZE];
@@ -733,53 +797,8 @@ void securid_compute_tokencode(struct securid_token *t, time_t now,
 int securid_encode_token(const struct securid_token *t, const char *pass,
 	const char *devid, char *out)
 {
-	struct securid_token newt = *t;
-	uint8_t d[MAX_TOKEN_BITS / 8 + 2];
-	uint8_t key_hash[AES_BLOCK_SIZE];
-	int rc;
-
-	/* empty password means "no password" */
-	if (pass && !strlen(pass))
-		pass = NULL;
-	if (devid && !strlen(devid))
-		devid = NULL;
-
-	rc = generate_key_hash(key_hash, pass, devid, &newt.device_id_hash,
-			       &newt);
-	if (rc)
-		return rc;
-
-	if (pass)
-		newt.flags |= FL_PASSPROT;
-	else
-		newt.flags &= ~FL_PASSPROT;
-
-	if (devid)
-		newt.flags |= FL_SNPROT;
-	else
-		newt.flags &= ~FL_SNPROT;
-
-	memset(d, 0, sizeof(d));
-	aes128_ecb_encrypt(key_hash, newt.dec_seed, newt.enc_seed);
-	memcpy(d, newt.enc_seed, AES_KEY_SIZE);
-
-	set_bits(d, 128, 16, newt.flags);
-	set_bits(d, 144, 14, newt.exp_date);
-	set_bits(d, 159, 15, securid_shortmac(newt.dec_seed, AES_KEY_SIZE));
-	set_bits(d, 174, 15, newt.device_id_hash);
-
-	sprintf(out, "%d%s", newt.version, newt.serial);
-	bits_to_numoutput(d, &out[BINENC_OFS], BINENC_BITS);
-
-	set_bits(d, 0, 15, securid_shortmac(out, CHECKSUM_OFS));
-	bits_to_numoutput(d, &out[CHECKSUM_OFS], CHECKSUM_BITS);
-
-	return ERR_NONE;
+	return v2_encode_token(t, pass, devid, out);
 }
-
-/********************************************************************
- * Misc operations
- ********************************************************************/
 
 int securid_random_token(struct securid_token *t)
 {
