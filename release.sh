c6020e854ca88ba779bbd5cfa361fdc68c0ae401
Kevin Cernekee <cernekee@gmail.com>
2014-06-21 11:18:12 -0700

    release.sh: Work around autotools search path wackiness
    When we test our release candidate tarball, we do not want autotools to
    pick up any files from the git repo.  So "bury" the build directory a few
    levels deeper under tmp.build/.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- release.sh
+++ release.sh
@@ -5,20 +5,28 @@ gpgkey="BC0B0D65"
 set -ex
 set -o pipefail
 
-rm -rf tmp.rel tmp.build stoken-*.tar.gz stoken-*.tar.gz.asc
-git clone . tmp.rel
+# autotools will search .:..:../.. for support files
+# let's make sure it can't search our repo, so we know it is getting all
+# required files from the release tarball
+builddir=tmp.build/a/b/c/d
+reldir=tmp.build/w/x/y/z
+repodir=`pwd`
 
-pushd tmp.rel
+rm -rf tmp.build stoken-*.tar.gz stoken-*.tar.gz.asc
+mkdir -p $reldir
+git clone . $reldir
+
+pushd $reldir
 ./autogen.sh
 ./configure
 fakeroot make dist
 tarball=$(ls -1 stoken-*.tar.gz)
-mv $tarball ../
+mv $tarball $repodir
 popd
 
-mkdir tmp.build
-pushd tmp.build
-tar -zxf ../$tarball --strip 1
+mkdir -p $builddir
+pushd $builddir
+tar -zxf $repodir/$tarball --strip 1
 ./configure --with-gtk
 make
 make distclean
@@ -28,7 +36,7 @@ make install DESTDIR=`pwd`/pfx
 make clean
 popd
 
-rm -rf tmp.rel tmp.build
+rm -rf tmp.build
 
 if gpg --list-secret-keys $gpgkey >& /dev/null; then
 	gpg --yes --armor --detach-sign --default-key $gpgkey $tarball
