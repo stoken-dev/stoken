c4d79ffbf5053e44be4b64da22b1b7fb6a51daf2
Kevin Cernekee <cernekee@gmail.com>
2014-11-29 19:51:38 -0800

    common: Don't #include <sys/mman.h> if we don't use mlockall()
    Windows systems do not have this header file, so it will generate
    a compile error.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- src/common.c
+++ src/common.c
@@ -26,9 +26,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+#ifdef HAVE_MLOCKALL
+#include <sys/mman.h>
+#endif
 
 #include "common.h"
 #include "securid.h"
